b0ae9a2ee23310f73bc9e1f9a259f6bea53ebb2e
Aivan <dominguezmiguel715@gmail.com>
2023-07-20 21:45:18 -0700

    Componente Grammar añadido.

---
+++ scripts/components/grammar.lua
@@ -0,0 +1,27 @@
+GRAMMATICAL_NUMBER =
+{
+  PLURAL = "plural",
+  SINGULAR = "singular"
+}
+
+GENDER =
+{
+  MALE = "male",
+  FEMALE = "female"
+}
+
+local Grammar = Class(function(self, inst)
+  self.inst = inst
+  self.grammaticalnumber = nil
+  self.gender = nil
+end)
+
+function Grammar:SetGrammaticalNumber(grammaticalNumber)
+  self.grammaticalnumber = grammaticalNumber or "UNKNOWN"
+end
+
+function Grammar:SetGender(gender)
+  self.gender = gender or "UNKNOWN"
+end
+
+return Grammar
