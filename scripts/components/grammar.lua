51c63d57d1e451e747beac6cb06156977ff9119d
Aivan <dominguezmiguel715@gmail.com>
2023-07-24 14:57:07 -0700

    Métodos OnSave y OnLoad añadidos.

--- scripts/components/grammar.lua
+++ scripts/components/grammar.lua
@@ -12,4 +12,30 @@ function Grammar:SetGender(gender)
   self.gender = gender or "UNKNOWN"
 end
 
+function Grammar:OnSave()
+  local data = {}
+
+  if self.grammaticalnumber then
+    data["grammaticalnumber"] = self.grammaticalnumber
+  end
+
+  if self.gender then
+    data["gender"] = self.gender
+  end
+
+  return data
+end
+
+function Grammar:OnLoad(data)
+  if data then
+    if data.grammaticalnumber then
+      self:SetGrammaticalNumber(data.grammaticalnumber)
+    end
+
+    if data.gender then
+      self:SetGender(data.gender)
+    end
+  end
+end
+
 return Grammar
