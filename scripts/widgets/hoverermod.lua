fd44ce47c324cc2309422d215ace56f53d5e8f5c
Aivan <dominguezmiguel715@gmail.com>
2023-07-25 16:45:00 -0700

    Función stackString removida y condicional para apilables modificada.

--- scripts/widgets/hoverermod.lua
+++ scripts/widgets/hoverermod.lua
@@ -1,21 +1,8 @@
+local subfmt = _G.subfmt
+local stackStyles = _G.stackStyles
 local modConfigurationName = _G.ModIndex:GetModConfigurationName("Traducción al Español")
 local stackStyleConfig = GetModConfigData("stackStyle", modConfigurationName)
-
-local function stackString(lmb)
-  local stackSize = tostring(lmb.target.components.stackable.stacksize)
-
-  if stackStyleConfig == "parenthesis" then
-    return "(" .. stackSize .. ")"
-  end
-
-  local char = nil
-  if stackStyleConfig == "mathematician" then
-    char = "× "
-  end
-
-  return (char or "x") .. stackSize
-end
-
+local stackStyle = stackStyles[stackStyleConfig] or stackStyles.default
 local HoverText = require "widgets/hoverer"
 local OnUpdateOriginal = HoverText.OnUpdate or function() return "" end
 
@@ -26,15 +13,17 @@ function HoverText:OnUpdate()
   local lmb = self.owner.components.playercontroller:GetLeftMouseAction()
 
   if str ~= "" and lmb and lmb.target then
-    if lmb.target.components.stackable and lmb.target.components.stackable.stacksize > 1 then
-      str = str:gsub(" x" .. lmb.target.components.stackable.stacksize, " " .. stackString(lmb))
+    if lmb.target.components.stackable and lmb.target.components.stackable.stacksize > 1 and stackStyleConfig ~= "default" then
+      local stack = lmb.target.components.stackable.stacksize
+
+      str = str:gsub("x" .. stack, subfmt(stackStyle, { stack = stack }))
     end
 
     local adjective = lmb.target:GetAdjective()
     if adjective then
       if showAdjectivesConfig then
         local name = lmb.target:GetDisplayName() or (lmb.target.components.named and lmb.target.components.named.name)
-  
+
         str = str:gsub(adjective .. " " .. name, ConstructAdjectivedName(lmb.target, name, adjective))
       else
         str = str:gsub(adjective .. " ", "")
