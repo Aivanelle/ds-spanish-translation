8d862c2979c7f83fbc7a696fcaa96d7a62130fd8
Aivan <dominguezmiguel715@gmail.com>
2023-07-27 15:03:24 -0700

    Sentencia if añadida.

--- scripts/widgets/inventorybarmod.lua
+++ scripts/widgets/inventorybarmod.lua
@@ -1,19 +1,21 @@
 local Inv = require "widgets/inventorybar"
 local GetOriginalDescriptionString = Inv.GetDescriptionString
 
-function Inv:GetDescriptionString(item)
-  local str = GetOriginalDescriptionString(self, item)
-  local adjective = item:GetAdjective()
-
-  if str ~= "" and adjective then
-    if showAdjectivesConfig then
-      local name = item:GetDisplayName()
-
-      return str:gsub(escape_lua_pattern(adjective .. " " .. name), ConstructAdjectivedName(item, name, adjective))
-    else
-      return str:gsub(escape_lua_pattern(adjective .. " "), "")
+if GetOriginalDescriptionString and anyDLCEnabled then
+  function Inv:GetDescriptionString(item)
+    local str = GetOriginalDescriptionString(self, item)
+    local adjective = item:GetAdjective()
+  
+    if str ~= "" and adjective then
+      if showAdjectivesConfig then
+        local name = item:GetDisplayName()
+  
+        return str:gsub(escape_lua_pattern(adjective .. " " .. name), ConstructAdjectivedName(item, name, adjective))
+      else
+        return str:gsub(escape_lua_pattern(adjective .. " "), "")
+      end
     end
+  
+    return str
   end
-
-  return str
 end
