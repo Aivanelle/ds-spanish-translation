48aa293ab104f0e3faaabf2bdda50c2cf173e55c
Aivan <dominguezmiguel715@gmail.com>
2023-08-02 00:09:01 -0700

    Método GetAdjective reescrito y método GetGrammaticalAdjective añadido.

--- scripts/components/perishablemod.lua
+++ scripts/components/perishablemod.lua
@@ -1,19 +1,24 @@
 local Perishable = require "components/perishable"
+local GetOriginalAdjective = Perishable.GetAdjective
 
 function Perishable:GetAdjective()
-  if self.inst.components.edible then
-    if self:IsStale() then
-      return self.inst:HasTag("frozen") and STRINGS.UI.HUD.STALE_FROZEN or
-        (self.inst:GetGrammaticalSuffix(STRINGS.SUFFIX.PERISHABLE.STALE) or STRINGS.UI.HUD.STALE)
-    elseif self:IsSpoiled() then
-      return self.inst:HasTag("frozen") and STRINGS.UI.HUD.STALE_FROZEN or
-        (self.inst:GetGrammaticalSuffix(STRINGS.SUFFIX.PERISHABLE.SPOILED) or STRINGS.UI.HUD.SPOILED)
-    end
-  elseif self.inst.components.eater then
-    if self:IsStale() then
-      return self.inst:GetGrammaticalSuffix(STRINGS.SUFFIX.CREATURE.HUNGRY) or STRINGS.UI.HUD.HUNGRY
-    elseif self:IsSpoiled() then
-      return self.inst:GetGrammaticalSuffix(STRINGS.SUFFIX.CREATURE.STARVING) or STRINGS.UI.HUD.STARVING
+  local adjective = GetOriginalAdjective(self)
+
+  if self.inst.components.edible and not self:IsFresh() and self.inst:HasTag("frozen") then
+    return STRINGS.UI.HUD[self:IsStale() and "STALE_FROZEN" or "SPOILED_FROZEN"]
+  end
+
+  return adjective
+end
+
+function Perishable:GetGrammaticalAdjective()
+  if self.inst.components.grammar then
+    if self.inst.components.edible and not self:IsFresh() then
+      if self.inst:HasTag("frozen") then return self:GetAdjective() end
+
+      return self.inst:GetGrammaticalSuffix(STRINGS.SUFFIX.PERISHABLE[self:IsStale() and "STALE" or "SPOILED"])
+    elseif self.inst.components.eater and not self:IsFresh() then
+      return self.inst:GetGrammaticalSuffix(STRINGS.SUFFIX.CREATURE[self:IsStale() and "HUNGRY" or "STARVING"])
     end
   end
 end
