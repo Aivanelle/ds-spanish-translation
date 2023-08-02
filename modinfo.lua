69d157cb1a66449e2f2180ddc51f465e487b34f7
Aivan <dominguezmiguel715@gmail.com>
2023-08-01 21:22:25 -0700

    Cambio de versión.

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,6 @@
 name = "Traducción al Español"
 author = "Aivan"
-version = "2.15.3"
+version = "2.16"
 description = "\nTraducción de Don't Starve y sus DLC: Reign of Giants, Shipwrecked y Hamlet. Personajes en progreso." ..
   "\nVersión " .. version
 
